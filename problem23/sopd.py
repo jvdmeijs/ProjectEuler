fc922add0f96382cd75331b0415a7b63a8583e5d
Joost van der Meijs <j.v.d.meijs@student.tue.nl>
2014-03-24 14:26:27 +0100

    Sum of proper divisors

---
+++ problem23/sopd.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+def d(n):
+    s = 1
+    t = sqrt(n)
+    for i in range(2, int(t)+1):
+        if n % i == 0: s += i + n/i
+    if t == int(t): s -= t    #correct s if t is a perfect square
+    return s
